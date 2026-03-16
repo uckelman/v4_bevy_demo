1121011d3b06f71540f3b6ac7ab7c97a9cf0ef36
Joel Uckelman <uckelman@nomic.net>
2026-03-16 18:17:15 +0000

    s/GroupProxy/GroupProxyOut

--- src/log.rs
+++ src/log.rs
@@ -700,14 +700,19 @@ where
 }
 
 // TODO: would it make sense to implement PartialOrd for the stop point?
-struct GroupProxy<'e, 's, 'w>(Entity, &'e Edits, &'s [(Entity, usize)], &'w DeferredWorld<'w>);
+struct GroupProxyOut<'e, 's, 'w>(
+    Entity,
+    &'e Edits,
+    &'s [(Entity, usize)],
+    &'w DeferredWorld<'w>
+);
 
-impl Serialize for GroupProxy<'_, '_, '_> {
+impl Serialize for GroupProxyOut<'_, '_, '_> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer
     {
-        let GroupProxy(entity, edits, stops, world) = &self;
+        let GroupProxyOut(entity, edits, stops, world) = &self;
 
         let mut edit_query = world.try_query::<(EntityRef, &EditType)>()
             .expect("no query");
@@ -727,7 +732,7 @@ impl Serialize for GroupProxy<'_, '_, '_> {
                 EditType::Delete => seq.serialize_edit::<DeleteEdit>(eref)?,
                 EditType::Flip => seq.serialize_edit::<FlipEdit>(eref)?,
                 EditType::Group => seq.serialize_element(
-                    &GroupProxy(
+                    &GroupProxyOut(
                         e,
                         eref.get::<Edits>().expect("edit type mismatch"),
                         // peel off this level for the redo boundary
@@ -779,7 +784,7 @@ pub fn serialize_edits(world: DeferredWorld) -> Result
         stops.push((e, idx));
     }
 
-    let g = GroupProxy(root_entity, root_edits, &stops, &world);
+    let g = GroupProxyOut(root_entity, root_edits, &stops, &world);
 
     serde_json::to_writer(&mut writer, &g)?;
     writeln!(&mut writer)?;
